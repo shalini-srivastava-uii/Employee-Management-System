9ba4c691b25fbe507f0335c83eee6c01865a928e
shalini-srivastava-uii <105551917+shalini-srivastava-uii@users.noreply.github.com>
2022-09-12 13:21:20 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,25 @@
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Employee
+
+        
+
+# Create your views here.
+def home(request):
+    employees = Employee.objects.all()
+    return render(request,'ems/home.html', {'employees': employees})
+
+def employee_detail(request, employee_id):
+        try:
+            employee = Employee.objects.get(id=employee_id)
+        except Employee.DoesNotExist:
+                raise Http404('Employee not found')
+        return render(request, 'ems/employee_detail.html', {'employee': employee})
+        
+
+
+
+
+#def employee_detail(request, employee_id):
+    #return HttpResponse(f'<p>employee_detail view with id {employee_id}</p>')
